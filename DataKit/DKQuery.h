4151723a6b8ac39a780e67831fc5b29ce457c5ff
Erik Aigner <erik@chocomoko.com>
2012-03-11 20:29:33 +0100

    updated doc comments

--- DataKit/DKQuery.h
+++ DataKit/DKQuery.h
@@ -20,6 +20,12 @@ typedef void (^DKQueryResultCountBlock)(NSUInteger count, NSError *error);
  Class for performing queries on entity collections.
  */
 @interface DKQuery : NSObject
+
+/** @name Options */
+
+/**
+ The entity name to perform the query on
+ */
 @property (nonatomic, copy, readonly) NSString *entityName;
 
 /**
@@ -42,244 +48,261 @@ typedef void (^DKQueryResultCountBlock)(NSUInteger count, NSError *error);
  */
 @property (nonatomic, assign) DKCachePolicy cachePolicy;
 
-/*!
- @param entityName The name of the entity to fetch.
- @abstract Initializes a new query for the given entity name.
+/** @name Creating and Initializing Queries */
+
+/**
+ Creates a new query for the given entity name
+ @param entityName The name of the entity to query
+ @return The initialized query
  */
 + (DKQuery *)queryWithEntityName:(NSString *)entityName;
 
-/*!
+/**
+ Initializes a new query for the given entity name
  @param entityName The name of the entity to fetch.
- @abstract Initializes a new query for the given entity name.
+ @return The initialized query
  */
 - (id)initWithEntityName:(NSString *)entityName;
 
-/*!
- @abstract Resets all query conditions
- */
-- (void)reset;
+/** @name Logical Operations */
 
-/*!
+/**
+ Add an OR condition to the query using the proxy object
  @return or The OR proxy object.
- @abstract Add an OR condition using the proxy object.
  */
 - (DKQuery *)or;
 
-/*!
+/**
+ Add an AND condition using the proxy object
  @return and The AND proxy object.
- @abstract Add an AND condition using the proxy object.
  */
 - (DKQuery *)and;
 
-/*!
+/** @name Ordering */
+
+/**
+ Sorts the query in ascending order by key
  @param key The sort key
- @abstract Sorts the query in ascending order by key.
  */
 - (void)orderAscendingByKey:(NSString *)key;
 
-/*!
+/**
+ Sorts the query in descending order by key
  @param key The sort key
- @abstract Sorts the query in descending order by key.
  */
 - (void)orderDescendingByKey:(NSString *)key;
 
-/*!
+/** @name Conditions */
+
+/**
+ Adds an equal condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds an equal condition to the query.
  */
 - (void)whereKey:(NSString *)key equalTo:(id)object;
 
-/*!
+/**
+ Adds a less-than condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds a less-than condition to the query.
  */
 - (void)whereKey:(NSString *)key lessThan:(id)object;
 
-/*!
+/**
+ Adds a less-than-or-equal condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds a less-than-or-equal condition to the query.
  */
 - (void)whereKey:(NSString *)key lessThanOrEqualTo:(id)object;
 
 /*!
+ Adds a greater-than condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds a greater-than condition to the query.
  */
 - (void)whereKey:(NSString *)key greaterThan:(id)object;
 
-/*!
+/**
+ Adds a greater-than-or-equal condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds a greater-than-or-equal condition to the query.
  */
 - (void)whereKey:(NSString *)key greaterThanOrEqualTo:(id)object;
 
-/*!
+/**
+ Adds an not-equal condition to the query
  @param key The entity key
  @param object The condition object
- @abstract Adds an not-equal condition to the query.
  */
 - (void)whereKey:(NSString *)key notEqualTo:(id)object;
 
-/*!
+/**
+ Adds an contained-in condition to the query
  @param key The entity key
  @param array The objects to check
- @abstract Adds an contained-in condition to the query.
  @discussion The key value must be contained in the given array.
  */
 - (void)whereKey:(NSString *)key containedIn:(NSArray *)array;
 
-/*!
+/**
+ Adds an not-contained-in condition to the query
  @param key The entity key
  @param array The objects to check
- @abstract Adds an not-contained-in condition to the query.
- @discussion The key value must not be contained in the given array.
+ @discussion The key value must not be contained in the given array
  */
 - (void)whereKey:(NSString *)key notContainedIn:(NSArray *)array;
 
-/*!
+/**
+ Adds an contains-all condition to the query
  @param key The entity key
  @param array The objects to check
- @abstract Adds an contains-all condition to the query.
- @discussion The key value must contain all values in the given array.
+ @discussion The key value must contain all values in the given array
  */
 - (void)whereKey:(NSString *)key containsAllIn:(NSArray *)array;
 
-/*!
+/**
+ Matches the regex with no options set
  @param key The entity key
  @param regex The regex to match
- @abstract Matches the regex with no options set
+ @see <whereKey:matchesRegex:options:>
  */
 - (void)whereKey:(NSString *)key matchesRegex:(NSString *)regex;
 
-/*!
+/**
+ Matches the regex using the provided option mask
  @param key The entity key
  @param regex The regex to match
  @param options The regex options
- @abstract Matches the regex using the provided option mask
  */
 - (void)whereKey:(NSString *)key matchesRegex:(NSString *)regex options:(DKRegexOption)options;
 
-/*!
+/**
+ Checks if the object for key contains the string
  @param key The entity key
  @param string The string to match
- @abstract Checks if the object for key contains the string
  */
 - (void)whereKey:(NSString *)key containsString:(NSString *)string;
 
-/*!
+/**
+ Checks if the object for key has the given prefix
  @param key The entity key
  @param prefix The prefix string to match
- @abstract Checks if the object for key has the given prefix
  */
 - (void)whereKey:(NSString *)key hasPrefix:(NSString *)prefix;
 
-/*!
+/**
+ Checks if the object for key has the given suffix
  @param key The entity key
  @param suffix The suffix string to match
- @abstract Checks if the object for key has the given suffix
  */
 - (void)whereKey:(NSString *)key hasSuffix:(NSString *)suffix;
 
-/*!
+/**
+ Checks if the entity key exists
  @param key The entity key
- @abstract Checks if the entity key exists.
  */
 - (void)whereKeyExists:(NSString *)key;
 
-/*!
+/**
+ Checks if the entity key does not exist
  @param key The entity key
- @abstract Checks if the entity key does not exist.
  */
 - (void)whereKeyDoesNotExist:(NSString *)key;
 
 - (void)includeKey:(NSString *)key UNAVAILABLE_ATTRIBUTE; // UNIMPLEMENTED
 
-/*!
+/** @name Executing Queries */
+
+/**
+ Finds all matching entities
  @return The matching entities
- @abstract Finds all matching entities.
  */
 - (NSArray *)findAll;
 
-/*!
- @param error Error object if error occurred
+/**
+ Finds all matching entities
+ @param error The error object to set on error
  @return The matching entities
- @abstract Finds all matching entities.
  */
 - (NSArray *)findAll:(NSError **)error;
 
-/*!
+/**
+ Finds all matching entities in the background and returns them to the callback block
  @param block The result callback
- @abstract Finds all matching entities in background and returns them to the callback block.
  */
 - (void)findAllInBackgroundWithBlock:(DKQueryResultsBlock)block;
 
-/*!
- @return Returns the matched entity
- @abstract Finds the first matching entity.
+/**
+ Finds the first matching entity
+ @return The matched entity
  @exception NSInternalInconsistencyException Raises an exception if a map reduce is set
  */
 - (DKEntity *)findOne;
 
-/*!
+/**
+ Finds the first matching entity
  @param error The error object written on error
- @return Returns the matched entity
- @abstract Finds the first matching entity.
+ @return The matched entity
  @exception NSInternalInconsistencyException Raises an exception if a map reduce is set
  */
 - (DKEntity *)findOne:(NSError **)error;
 
-/*!
+/**
+ Finds the first matching entity in the background and returns it to the callback block
  @param block The result callback block
- @abstract Finds the first matching entity in the background and returns it to the callback block.
  @exception NSInternalInconsistencyException Raises an exception if a map reduce is set
  */
 - (void)findOneInBackgroundWithBlock:(DKQueryResultBlock)block;
 
-/*!
+/**
+ Finds an entity by it's unique ID
  @param entityId The entity ID to find
  @return Returns the entity with the matching ID
- @abstract Finds an entity by it's unique ID.
  */
 - (DKEntity *)findById:(NSString *)entityId;
 
-/*!
+/**
+ Finds an entity by it's unique ID
  @param entityId The entity ID to find
  @param error The error object that is written on error
- @return Returns the entity with the matching ID
- @abstract Finds an entity by it's unique ID.
+ @return The entity with the matching ID
  */
 - (DKEntity *)findById:(NSString *)entityId error:(NSError **)error;
 
-/*!
+/**
+ Finds an entity by it's unique ID in the background and returns it to the callback block
  @param entityId The entity ID to find
  @param block The result callback block
- @abstract Finds an entity by it's unique ID in the background and returns it to the callback block.
  */
 - (void)findById:(NSString *)entityId inBackgroundWithBlock:(DKQueryResultBlock)block;
 
-/*!
+/** @name Aggregation */
+
+/**
+ Counts the entities matching the query
  @return The matched entity count
- @abstract Counts the entities matching the query.
  */
 - (NSInteger)countAll;
 
-/*!
+/**
+ Counts the entities matching the query
  @param error The error object that is written on error
  @return The matched entity count
- @abstract Counts the entities matching the query.
  */
 - (NSInteger)countAll:(NSError **)error;
 
-/*!
+/**
+ Counts the entities matching the query in the background and returns the result to the block
  @param block The result callback block
- @abstract Counts the entities matching the query in the background and returns the result to the block
  */
 - (void)countAllInBackgroundWithBlock:(DKQueryResultCountBlock)block;
+
+/** @name Resetting Conditions */
+
+/**
+ Resets all query conditions
+ */
+- (void)reset;
 
 + (id)new UNAVAILABLE_ATTRIBUTE;
 - (id)init UNAVAILABLE_ATTRIBUTE;
