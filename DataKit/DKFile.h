23a349c983c4fd422027965f490ad0c460ae12c3
Erik Aigner <erik@chocomoko.com>
2012-03-13 21:15:31 +0100

    defined DKFile interface

---
+++ DataKit/DKFile.h
@@ -0,0 +1,45 @@
+//
+//  DKFile.h
+//  DataKit
+//
+//  Created by Erik Aigner on 13.03.12.
+//  Copyright (c) 2012 chocomoko.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef void (^DKFileSaveResultBlock)(BOOL success, NSError *error);
+typedef void (^DKFileLoadResultBlock)(NSData *data, NSError *error);
+typedef void (^DKFileProgressBlock)(double progress);
+
+@interface DKFile : NSObject
+@property (nonatomic, assign, readonly) BOOL isDirty;
+@property (nonatomic, assign, readonly) BOOL hasData;
+@property (nonatomic, copy, readonly) NSURL *URL;
+@property (nonatomic, copy, readonly) NSString *name;
+
+/** @name Creating Files */
+
++ (DKFile *)fileWithData:(NSData *)data;
++ (DKFile *)fileWithData:(NSData *)data name:(NSString *)name;
+
+/** @name Saving Files */
+
+- (BOOL)save;
+- (BOOL)save:(NSError **)error;
+- (void)saveInBackgroundWithBlock:(DKFileSaveResultBlock)block;
+- (void)saveInBackgroundWithBlock:(DKFileSaveResultBlock)block progressBlock:(DKFileProgressBlock)progressBlock;
+
+/** @name Loading Data */
+
+- (NSData *)loadData;
+- (NSData *)loadData:(NSError **)error;
+- (void)loadDataInBackgroundWithBlock:(DKFileLoadResultBlock)block;
+- (void)loadDataInBackgroundWithBlock:(DKFileLoadResultBlock)block progressBlock:(DKFileProgressBlock)progressBlock;
+
+/** @name Aborting */
+
+- (void)abort;
+
+@end
---
+++ DataKit/DKFile.m
@@ -0,0 +1,13 @@
+//
+//  DKFile.m
+//  DataKit
+//
+//  Created by Erik Aigner on 13.03.12.
+//  Copyright (c) 2012 chocomoko.com. All rights reserved.
+//
+
+#import "DKFile.h"
+
+@implementation DKFile
+
+@end
